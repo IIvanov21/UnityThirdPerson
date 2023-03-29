c0b32091b45d007e5c19e7ed29be47af2837273c
iivanov21 <iivanov21@hotmail.com>
2023-03-29 13:57:39 +0100

    StateMachine + Imported Animations

---
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachine : StateMachine
+{
+    [field:SerializeField]
+    public InputReader InputReader
+    {
+        get; private set;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        SwitchState(new PlayerTestState(this));        
+    }
+
+}
---
+++ Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTestState : PlayerBaseState
+{
+    
+
+    public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine){    }
+
+    public override void Enter()
+    {
+        Debug.Log("Enter");
+        
+
+    }
+
+    public override void Exit()
+    {
+        Debug.Log("Exit");
+        
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        Vector3 movement = new Vector3();
+        movement.x = stateMachine.InputReader.MovementValue.x;
+        movement.y = 0;
+        movement.z = stateMachine.InputReader.MovementValue.y;
+        stateMachine.transform.Translate(movement*deltaTime);
+    }
+
+   
+}
