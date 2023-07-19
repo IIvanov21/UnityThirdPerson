3bce42672e58b99070a470460150c931bd47bbd7
iivanov21 <iivanov21@hotmail.com>
2023-07-19 09:09:28 +0100

    Comments added

--- Assets/Scripts/StateMachines/Player/PlayerBaseState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerBaseState.cs
@@ -6,41 +6,47 @@ public abstract class PlayerBaseState : State
 {
     protected PlayerStateMachine stateMachine;
 
+    // Constructor that takes a PlayerStateMachine instance
     public PlayerBaseState(PlayerStateMachine stateMachine)
     {
         this.stateMachine = stateMachine;
     }
 
+    // Move the player based on the motion vector and delta time
     protected void Move(Vector3 motion, float deltaTime)
     {
-        stateMachine.Controller.Move((motion+stateMachine.ForceReceiver.Movement) * deltaTime);
+        stateMachine.Controller.Move((motion + stateMachine.ForceReceiver.Movement) * deltaTime);
     }
 
-    protected void Move( float deltaTime)
+    // Move the player with zero motion vector and delta time
+    protected void Move(float deltaTime)
     {
         stateMachine.Controller.Move((Vector3.zero + stateMachine.ForceReceiver.Movement) * deltaTime);
     }
 
+    // Rotate the player to face the current target (if any)
     protected void FaceTarget()
     {
         if (stateMachine.Targeter.CurrentTarget == null) return;
 
         Vector3 facingVector = stateMachine.Targeter.CurrentTarget.transform.position - stateMachine.transform.position;
 
+        // Set the y-component to zero to ensure rotation is only in the horizontal plane
         facingVector.y = 0.0f;
 
         stateMachine.transform.rotation = Quaternion.LookRotation(facingVector);
     }
 
+    // Switch to the appropriate state based on the presence of a target
     protected void ReturnToLocomotion()
     {
-        if(stateMachine.Targeter.CurrentTarget != null)
+        if (stateMachine.Targeter.CurrentTarget != null)
         {
             stateMachine.SwitchState(new PlayerTargetingState(stateMachine));
         }
         else
         {
-            stateMachine.SwitchState(new PlayerFreeLookState(stateMachine));    
+            stateMachine.SwitchState(new PlayerFreeLookState(stateMachine));
         }
     }
 }
