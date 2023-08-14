19d01665f49186fe4945420ff4be5ad9cf88d0a5
iivanov21 <iivanov21@hotmail.com>
2023-08-14 16:33:14 +0100

    Adding commenting to the files

--- Assets/Scripts/InputReader.cs
+++ Assets/Scripts/InputReader.cs
@@ -6,80 +6,106 @@ using UnityEngine.InputSystem;
 
 public class InputReader : MonoBehaviour, Controls.IPlayerActions
 {
-
+    // Store the player's movement input as a Vector2.
     public Vector2 MovementValue { get; private set; }
+    // Store whether the player is attacking or not.
     public bool IsAttacking { get; private set; }
+    // Store whether the player is blocking or not.
     public bool IsBlocking { get; private set; }
 
+    // Define events that other scripts can subscribe to.
     public event Action JumpEvent;
     public event Action DodgeEvent;
     public event Action TargetEvent;
 
+    // Store the Input System controls.
     private Controls controls;
 
-
-
+    // Initialize when the script starts.
     private void Start()
     {
+        // Create a new instance of the Controls class.
         controls = new Controls();
+        // Set the callbacks for player actions to this script.
         controls.Player.SetCallbacks(this);
+        // Enable the player input.
         controls.Player.Enable();
     }
 
+    // Clean up when the script is destroyed.
     private void OnDestroy()
     {
+        // Disable the player input.
         controls.Player.Disable();
     }
 
+    // Triggered when the jump input is performed.
     public void OnJump(InputAction.CallbackContext context)
     {
-        if(!context.performed) { return; }
+        // If the input is not performed, do nothing.
+        if (!context.performed) { return; }
+        // Invoke the JumpEvent, notifying subscribers.
         JumpEvent?.Invoke();
     }
 
+    // Triggered when the dodge input is performed.
     public void OnDodge(InputAction.CallbackContext context)
     {
-        if(!context.performed) { return; };
+        // If the input is not performed, do nothing.
+        if (!context.performed) { return; };
+        // Invoke the DodgeEvent, notifying subscribers.
         DodgeEvent?.Invoke();
     }
 
+    // Triggered when the movement input is changed.
     public void OnMove(InputAction.CallbackContext context)
     {
+        // Read the value of the movement input and store it.
         MovementValue = context.ReadValue<Vector2>();
+        // Output the movement value to the console for debugging.
         Debug.Log(MovementValue);
     }
 
+    // Triggered when the look input is changed.
     public void OnLook(InputAction.CallbackContext context)
     {
-
+        // This method is currently empty.
+        // You can implement it if needed for camera control.
     }
 
+    // Triggered when the target input is performed.
     public void OnTarget(InputAction.CallbackContext context)
     {
+        // If the input is not performed, do nothing.
         if (!context.performed) { return; };
+        // Invoke the TargetEvent, notifying subscribers.
         TargetEvent?.Invoke();
     }
 
-    
-
+    // Triggered when the attack input is performed or canceled.
     public void OnAttack(InputAction.CallbackContext context)
     {
+        // If the input is performed, set IsAttacking to true.
         if (context.performed)
         {
             IsAttacking = true;
         }
+        // If the input is canceled, set IsAttacking to false.
         else if (context.canceled)
         {
             IsAttacking = false;
         }
     }
 
+    // Triggered when the block input is performed or canceled.
     public void OnBlock(InputAction.CallbackContext context)
     {
+        // If the input is performed, set IsBlocking to true.
         if (context.performed)
         {
             IsBlocking = true;
         }
+        // If the input is canceled, set IsBlocking to false.
         else if (context.canceled)
         {
             IsBlocking = false;
--- Assets/Scripts/StateMachines/Player/PlayerTargetingState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerTargetingState.cs
@@ -4,82 +4,90 @@ using UnityEngine;
 
 public class PlayerTargetingState : PlayerBaseState
 {
+    // Hashes for Animator parameters
     private readonly int TargetingBlendTree = Animator.StringToHash("TargetingBlendTree");
-    private readonly int TargetingForward= Animator.StringToHash("TargetingForward");
+    private readonly int TargetingForward = Animator.StringToHash("TargetingForward");
     private readonly int TargetingRight = Animator.StringToHash("TargetingRight");
+
     private const float AnimatorDampTime = 0.1f;
-    
 
     public PlayerTargetingState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 
     public override void Enter()
     {
-        stateMachine.Animator.CrossFadeInFixedTime(TargetingBlendTree,AnimatorDampTime);
+        // Crossfade to targeting animation
+        stateMachine.Animator.CrossFadeInFixedTime(TargetingBlendTree, AnimatorDampTime);
+
+        // Subscribe to input events
         stateMachine.InputReader.TargetEvent += OnTarget;
         stateMachine.InputReader.DodgeEvent += OnDodge;
         stateMachine.InputReader.JumpEvent += OnJump;
-
     }
 
     public override void Exit()
     {
-        stateMachine.InputReader.TargetEvent += OnTarget;
+        // Unsubscribe from input events
+        stateMachine.InputReader.TargetEvent -= OnTarget;
         stateMachine.InputReader.DodgeEvent -= OnDodge;
         stateMachine.InputReader.JumpEvent -= OnJump;
-
     }
 
     public override void Tick(float deltaTime)
     {
+        // Handle state transitions based on input and conditions
 
         if (stateMachine.InputReader.IsAttacking)
         {
-            stateMachine.SwitchState(new PlayerAttackingState(stateMachine,0));
+            stateMachine.SwitchState(new PlayerAttackingState(stateMachine, 0));
             return;
         }
 
-        if(stateMachine.InputReader.IsBlocking)
+        if (stateMachine.InputReader.IsBlocking)
         {
             stateMachine.SwitchState(new PlayerBlockingState(stateMachine));
             return;
         }
 
-        if(stateMachine.Targeter.CurrentTarget == null)
+        if (stateMachine.Targeter.CurrentTarget == null)
         {
             stateMachine.SwitchState(new PlayerFreeLookState(stateMachine));
             return;
         }
 
+        // Calculate movement
         Vector3 movement = CalculateMovement(deltaTime);
 
+        // Move the player
         Move(movement * stateMachine.TargetingMovementSpeed, deltaTime);
 
-
+        // Orient the player towards the target
         FaceTarget();
 
+        // Update Animator parameters
         UpdateAnimator(deltaTime);
     }
 
-    private void OnTarget() 
+    private void OnTarget()
     {
+        // Cancel targeting and switch to free look state
         stateMachine.Targeter.Cancel();
-
-
         stateMachine.SwitchState(new PlayerFreeLookState(stateMachine));
     }
 
     private Vector3 CalculateMovement(float deltaTime)
     {
-        Vector3 movement=new Vector3();
+        // Calculate movement based on input
+        Vector3 movement = Vector3.zero;
 
         movement += stateMachine.transform.right * stateMachine.InputReader.MovementValue.x;
         movement += stateMachine.transform.forward * stateMachine.InputReader.MovementValue.y;
-        
+
         return movement;
     }
 
     private void UpdateAnimator(float deltaTime)
     {
+        // Update Animator parameters for movement
         if (stateMachine.InputReader.MovementValue == Vector2.zero)
         {
             stateMachine.Animator.SetFloat(TargetingRight, 0, AnimatorDampTime, deltaTime);
@@ -89,16 +97,17 @@ public class PlayerTargetingState : PlayerBaseState
 
         stateMachine.Animator.SetFloat(TargetingForward, stateMachine.InputReader.MovementValue.y, AnimatorDampTime, deltaTime);
         stateMachine.Animator.SetFloat(TargetingRight, stateMachine.InputReader.MovementValue.x, AnimatorDampTime, deltaTime);
-
     }
 
     private void OnDodge()
     {
-        stateMachine.SwitchState(new PlayerDodgingState(stateMachine,stateMachine.InputReader.MovementValue));
+        // Switch to dodging state
+        stateMachine.SwitchState(new PlayerDodgingState(stateMachine, stateMachine.InputReader.MovementValue));
     }
 
     private void OnJump()
     {
+        // Switch to jumping state
         stateMachine.SwitchState(new PlayerJumpingState(stateMachine));
     }
 }
